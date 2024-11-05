40cf39ab3089d1c7c25e831ca8b62636207fa8b5
codingrecipe1 <codingrecipe1@gmail.com>
2024-11-05 11:39:24 +0900

    mysql 연동

--- express/index.js
+++ express/index.js
@@ -1,9 +1,24 @@
 import express from "express";
+import mysql from "mysql";
 
 const app = express();
+app.use(express.json());
 
 app.listen(8000, () => {
   console.log("서버 시작!");
+});
+
+const db = mysql.createConnection({
+  host: "127.0.0.1",
+  user: "user_ex",
+  password: "1234",
+  port: "3306",
+  database: "db_ex",
+});
+
+db.connect((err) => {
+  console.log("db 연결 성공!");
+  console.log("err", err);
 });
 
 app.get("/", () => {
@@ -28,4 +43,15 @@ app.get("/:id", (req) => {
   console.log(req.params);
   const { id } = req.params;
   console.log(id);
+});
+
+app.post("/hello", () => {
+  console.log("hello post 요청");
+});
+
+app.post("/post-req", (req) => {
+  console.log(req.body);
+  console.log(req.body.name);
+  const { name, age } = req.body;
+  console.log(`name: ${name}, age: ${age}`);
 });
