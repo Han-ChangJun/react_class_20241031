66f486bd9409b3462718288c36daa5a6d026171e
codingrecipe1 <codingrecipe1@gmail.com>
2024-11-05 10:38:53 +0900

    path variable 요청

--- express/index.js
+++ express/index.js
@@ -5,3 +5,19 @@ const app = express();
 app.listen(8000, () => {
   console.log("서버 시작!");
 });
+
+app.get("/", () => {
+  console.log("기본주소 요청");
+});
+
+// hello get 요청이 오면 콘솔에 hello 주소 요청 출력
+app.get("/hello", () => {
+  console.log("hello 주소 요청");
+});
+
+// board/10, product/6
+app.get("/:id", (req) => {
+  console.log(req.params);
+  const { id } = req.params;
+  console.log(id);
+});
