06033cc4dde692d713bcf824ffb41b3f940a0b60
codingrecipe1 <codingrecipe1@gmail.com>
2024-11-05 10:48:30 +0900

    query string

--- express/index.js
+++ express/index.js
@@ -15,6 +15,14 @@ app.get("/hello", () => {
   console.log("hello 주소 요청");
 });
 
+app.get("/qs", (req) => {
+  console.log(req.query);
+  const { p1, p2 } = req.query;
+  console.log("p1: " + p1 + ", p2: " + p2);
+
+  console.log(`p1: ${p1}, p2: ${p2}`);
+});
+
 // board/10, product/6
 app.get("/:id", (req) => {
   console.log(req.params);
